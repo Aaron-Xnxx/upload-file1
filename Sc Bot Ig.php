329fd86df9585eb43100617c6359807e079244d9
Aaron-Id <78670593+Aaron-Id@users.noreply.github.com>
2021-02-07 09:59:05 +0700

    Add files via upload

---
+++ Sc Bot Ig.php
@@ -0,0 +1,11 @@
+$ pkg update
+$ pkg upgrade
+$ pkg install python2
+$ pkg install ruby
+$ gem install lolcat
+$ pkg install git
+$git clonehttps://github.com/Hanzelnutt/instabot
+$ cd instabot
+$ ls
+$ pip2 install -r requirements.txt
+$ bash instabot
